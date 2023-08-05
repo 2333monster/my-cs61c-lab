b25c1dde3e6c88a57165402c5e6e63f6c7fe026b
2333monster <93046074+2333monster@users.noreply.github.com>
2023-08-05 17:26:28 +0800

    init lab01

--- lab01/hello.c
+++ lab01/hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    void call();
     int i;
     int count = 0;
     int *p = &count;
@@ -8,7 +9,14 @@ int main(int argc, char *argv[]) {
     for (i = 0; i < 10; i++) {
         (*p)++; // Do you understand this line of code and all the other permutations of the operators? ;)
     }
-
+    // call();
     printf("Thanks for waddling through this program. Have a nice day.");
     return 0;
 }
+
+
+// void
+// call(){
+//     printf("dfdfdf\n");
+//     printf("dfhhllk\n");
+// }
--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,17 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if(head == NULL){
+        return 0;
+    }
+    node *tortoise = head;
+    node *hare = head->next;
+    while(tortoise != NULL && hare != NULL && hare->next != NULL){
+        if(tortoise == hare){
+            return 1;
+        }
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+    }   
     return 0;
 }
